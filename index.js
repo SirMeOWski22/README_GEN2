7bf2b56fc45775fe415e16139f9ee214f721bdf5
Morgan <160355750+SirMeOWski22@users.noreply.github.com>
2024-05-03 00:26:15 -0500

    updated the index.js with code for getting the answers written to the readme.txt. A sample readme.txt is available to show code worked.

--- index.js
+++ index.js
@@ -1,13 +1,109 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'Project Title:',
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+      type: 'input',
+      name: 'motivation',
+      message: 'Motivation behind the project.',
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+      type: 'input',
+      name: 'problem',
+      message: 'What problem does your project solve?',
+    },
 
-// Function call to initialize app
-init();
+    {
+      type: 'input',
+      name: 'learn',
+      message: 'What did you learn from this project?',
+    },
+
+    {
+      type: 'confirm',
+      name: 'addScreenshots',
+      message: 'Do you have any screenshots of your work to add?',
+    },
+
+    {
+      type: 'input',
+      name: 'screenshotFilePath',
+      message: 'Please provide the file path where your screenshots and videos are located:',
+      when: (answers) => answers.addScreenshots === true,
+    },
+
+    {
+      type: 'input',
+      name: 'contributorGuidelines',
+      message: 'What are some contributor guidelines, if the project is open source.',
+    },
+
+    {
+      type: 'input',
+      name: 'projectLicense',
+      message: 'Type of license under which the project is released.',
+    },
+
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Credits to individuals or organizations that contributed to the project.',
+    },
+
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+  ])
+  .then((answers) => {
+    const fileName = 'readme.txt';
+    const content = generateReadme(answers);
+    writeToFile(fileName, content);
+  });
+
+function generateReadme(answers) {
+  return `
+    ## ${answers.projectTitle}
+    
+    ## Motivation
+     ${answers.motivation}
+    
+    ## Problem 
+    ${answers.problem}
+
+    ## What Did I Learn 
+    ${answers.learn}
+
+    ## Screenshots and Videos 
+    ${answers.addScreenshots}
+    
+    ## Contributor Guidelines
+    ${answers.contributorGuidelines}
+    
+    ## License 
+    ${answers.projectLicense}
+    
+    ## Credits 
+    ${answers.credits}
+     
+    Contact: ${answers.email}`;
+}
+
+function writeToFile(fileName, content) {
+  fs.writeFile(fileName, content, (err) => {
+    if (err) {
+      console.error(`Error writing to ${fileName}: ${err}`);
+    } else {
+      console.log(`Content has been written to ${fileName} Successfully!`);
+    }
+  });
+}
---
+++ readme.txt
@@ -0,0 +1,25 @@
+
+    ## Readme Generator
+    
+    ## Motivation
+     I always have trouble with where to start when it comes to writing README files. I want to help other new developers by giving them a guide to follow.
+    
+    ## Problem 
+    This generator will provide new developers or any developer with a quick quide to get started with wrtiing a README file.
+
+    ## What Did I Learn 
+    I learned how to work with inquirer, node, and how to use prompt. I struggled with .then, but I am slowly getting the hang of it.
+
+    ## Screenshots and Videos 
+    true
+    
+    ## Contributor Guidelines
+    Commit with comments and provide ease and accessibility to the website.
+    
+    ## License 
+    None at this time.
+    
+    ## Credits 
+    I give big credit to my instructors, the internet because it provide valuable information and guidance, and of course myself for not quitting!
+     
+    Contact: amorgan680@gmail.com
